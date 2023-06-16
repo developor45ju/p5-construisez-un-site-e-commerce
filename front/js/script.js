be2f390f51e2b8feabbfe5b9aed8a6246be689b8
Julien Heng <julienheng0@gmail.com>
2023-06-16 15:09:49 +0200

    Add cards kanap in home page

---
+++ front/js/script.js
@@ -0,0 +1,16 @@
+fetch("http://localhost:3000/api/products")
+    .then(reponse => reponse.json())
+    .then(data => {
+        let putCardsProducts = "";
+        for (let i = 0;i < data.length;i++) {
+            putCardsProducts += `<a href="./product.html?id=${data[i]._id}">
+                                    <article>
+                                        <img src="${data[i].imageUrl}" alt="${data[i].altTxt}">
+                                        <h3 class="productName">${data[i].name}</h3>
+                                        <p class="productDescription">${data[i].description}</p>
+                                    </article>
+                                </a>`
+        }
+        document.querySelector("#items").innerHTML = putCardsProducts;
+    })
+    .catch(err => console.log(err));
