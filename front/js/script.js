27a65276419838e9992a15c5844afe4433236c9b
Julien Heng <julienheng0@gmail.com>
2023-06-22 10:44:24 +0200

    Transform function kanaps in class Kanaps

--- front/js/script.js
+++ front/js/script.js
@@ -1,22 +1,36 @@
 // Récupère les données de l'API et affiche dynamiquent sur la page d'accueil du site
 
-async function displayKanaps() {
-    try {
-        const response = await fetch("http://localhost:3000/api/products")
-        const kanaps = await response.json();
-        let putCardsProducts = "";
-        kanaps.foreach(kanap => {
-            putCardsProducts += `<a href="./product.html?id=${kanap._id}">
-                                    <article>
-                                        <img src="${kanap.imageUrl}" alt="${kanap.altTxt}">
-                                        <h3 class="productName">${kanap.name}</h3>
-                                        <p class="productDescription">${kanap.description}</p>
-                                    </article>
-                                </a>`
-        })
-        document.getElementById("items").innerHTML = putCardsProducts;
-    } catch(error) {
-        console.error(error)
+class Kanaps {
+    constructor() {
+        this.response = fetch('http://localhost:3000/api/products');
+        this.kanaps = null;
+        this.putCardsProducts = "";
+    }
+
+    async displayKanaps() {
+        try {
+            const RESPONSE = await this.response;   
+            const KANAPS = await RESPONSE.json();
+            KANAPS.forEach(kanap => {
+                this.putCardsProducts += `<a href="./product.html?id=${kanap._id}">
+                                            <article>
+                                                <img src="${kanap.imageUrl}" alt="${kanap.altTxt}">
+                                                <h3 class="productName">${kanap.name}</h3>
+                                                <p class="productDescription">${kanap.description}</p>
+                                            </article>
+                                        </a>`
+            })
+            document.getElementById('items').innerHTML = this.putCardsProducts;
+        } catch (error) {
+            error = 'L\'API n\'a pas pu être récupéré';
+            console.error(error);
+        }
+    }
+
+    getKanaps() {
+        return this.displayKanaps();
     }
 }
-displayKanaps();
+
+const ALL_KANAPS = new Kanaps();
+ALL_KANAPS.getKanaps();
