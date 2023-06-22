53d2bc59152bb0224744f2229945a1704f6358be
Julien Heng <julienheng0@gmail.com>
2023-06-22 08:58:38 +0200

    Modify code for cath API all products

--- front/js/script.js
+++ front/js/script.js
@@ -1,18 +1,22 @@
 // Récupère les données de l'API et affiche dynamiquent sur la page d'accueil du site
 
-fetch("http://localhost:3000/api/products")
-    .then(reponse => reponse.json())
-    .then(data => {
+async function displayKanaps() {
+    try {
+        const response = await fetch("http://localhost:3000/api/products")
+        const kanaps = await response.json();
         let putCardsProducts = "";
-        for (let i = 0;i < data.length;i++) {
-            putCardsProducts += `<a href="./product.html?id=${data[i]._id}">
+        kanaps.foreach(kanap => {
+            putCardsProducts += `<a href="./product.html?id=${kanap._id}">
                                     <article>
-                                        <img src="${data[i].imageUrl}" alt="${data[i].altTxt}">
-                                        <h3 class="productName">${data[i].name}</h3>
-                                        <p class="productDescription">${data[i].description}</p>
+                                        <img src="${kanap.imageUrl}" alt="${kanap.altTxt}">
+                                        <h3 class="productName">${kanap.name}</h3>
+                                        <p class="productDescription">${kanap.description}</p>
                                     </article>
                                 </a>`
-        }
-        document.querySelector("#items").innerHTML = putCardsProducts;
-    })
-    .catch(err => console.log(err));
+        })
+        document.getElementById("items").innerHTML = putCardsProducts;
+    } catch(error) {
+        console.error(error)
+    }
+}
+displayKanaps();
