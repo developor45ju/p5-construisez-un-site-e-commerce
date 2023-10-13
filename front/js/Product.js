812d7f52962f1158bba0d8669db87850d746579d
Julien Heng <julienheng0@gmail.com>
2023-10-13 10:32:46 +0200

    Fixed code

--- front/js/Product.js
+++ front/js/Product.js
@@ -30,6 +30,7 @@ export default class Product {
       .appendChild(newErrorMessage);
       return false;
     }
+    return true;
   }
   
   /**
@@ -53,6 +54,7 @@ export default class Product {
         .appendChild(newErrorMessage);
       return false;
     }
+    return true;
   }
 
   /**
